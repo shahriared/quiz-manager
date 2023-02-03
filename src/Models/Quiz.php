2a0d6c7655b2fb80336817bd2e1cf8660104bfc2
Shahriar Edalat Zadeh <shahriared@gmail.com>
2023-02-03 11:07:16 +0400

    feat: update quiz and quiz questions model.

--- src/Models/Quiz.php
+++ src/Models/Quiz.php
@@ -3,14 +3,19 @@
 namespace Shahriared\QuizManager\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Quiz extends Model
 {
+    use HasTranslations;
+
+    public $translatable = ['name', 'description'];
+
     protected $table = 'quizzes';
 
-    protected $fillable = ['name', 'description', 'is_published'];
+    protected $fillable = ['name', 'max_point', 'description', 'is_published'];
 
-    public function images()
+    public function questions()
     {
         return $this->hasMany(QuizQuestion::class, 'quiz_id');
     }
--- src/Models/QuizQuestion.php
+++ src/Models/QuizQuestion.php
@@ -3,10 +3,25 @@
 namespace Shahriared\QuizManager\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class QuizQuestion extends Model
 {
+    use HasTranslations;
+    
+    public $translatable = ['title', 'hint'];
+
     protected $table = 'quiz_questions';
 
-    protected $fillable = ['quiz_id', 'name', 'description', 'alt', 'image'];
+    protected $fillable = ['quiz_id', 'title', 'hint', 'type', 'options', 'is_published'];
+
+    protected $casts = [
+        'options' => 'array',
+    ];
+
+    public function quiz()
+    {
+        return $this->hasOne(Quiz::class, 'quiz_id');
+    }
+
 }
