2d046526f0d0067fdf6b69559c3ed5509cbe677a
Shahriar Edalat Zadeh <shahriared@gmail.com>
2023-02-03 11:06:45 +0400

    feat; update quizzes table migration.

--- database/migrations/create_quizzes_table.php
+++ database/migrations/create_quizzes_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
         Schema::create('quizzes', function (Blueprint $table) {
             $table->id();
             $table->longText('name');
+            $table->double('max_point')->nullable();
             $table->longText('description');
             $table->boolean('is_published');
             $table->timestamps();
