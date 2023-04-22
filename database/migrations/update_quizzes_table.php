d024f9e4d1bfa904ec11ad65e504ca48fc923e3c
Shahriar Edalat Zadeh <57944847+shahriared@users.noreply.github.com>
2023-04-22 09:03:12 +0400

    Create update_quizzes_table.php

---
+++ database/migrations/update_quizzes_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('quizzes', function (Blueprint $table) {
+            $table->longText('url')->nullable()->after('desccription');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('quizzes', function (Blueprint $table) {
+            $table->dropColumn('url');
+        });
+    }
+};
